fea94cd21a3b0a70175f6999323333efb4707c46
vishnu <vishnu@sedintechnologies.com>
2024-08-21 03:35:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <div className="center">
+        <div className='countdown'>
+          <FlipClockCountdown className='clock' to={new Date('September 16, 2024 05:00:00').getTime()}
+            labels={['Days', 'Hours', 'Minutes', 'Seconds']}
+          >
+            I am married!
+          </FlipClockCountdown>
+        </div>
+
+        <div className="video-responsive">
+          <iframe
+            src={`https://www.youtube.com/embed/7DJY0fQ-s38`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Wedding invite"
+          />
+        </div>
+      </div>
+    </main>
   );
 }
 
