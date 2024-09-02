9bb2ed6e52c22ca3f2a1c6d29640019a7f5b7730
vishnu <vishnu@sedintechnologies.com>
2024-09-02 10:26:12 +0530

    router added

---
+++ src/Currency.js
@@ -0,0 +1,5 @@
+function Currency() {
+    return <>currency conversion</>
+}
+
+export default Currency;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Currency from './Currency';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/c",
+    element: <Currency />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
